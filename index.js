74132218229f9aaa119ae4b8e317010285e98cee
Jason Brennan <>
2021-04-15 14:47:22 +0000

    ADDED A CHUNK OF CODE

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-
+const routes = require('./lib/routes')
+const moves = require('./lib/moves')
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/', handleIndex)
-app.post('/start', handleStart)
-app.post('/move', handleMove)
-app.post('/end', handleEnd)
+app.get('/', routes.handleIndex)
+app.post('/start', routes.handleGameData)
+app.post('/move', routes.handleMove)
+app.post('/end', routes.handleGameData)
 
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
-
-function handleIndex(request, response) {
-  var battlesnakeInfo = {
-    apiversion: '1',
-    author: '',
-    color: '#888888',
-    head: 'shades',
-    tail: 'default'
-  }
-  response.status(200).json(battlesnakeInfo)
-}
-
-function handleStart(request, response) {
-  var gameData = request.body
-	console.log(gameData)
-
-  console.log('START')
-  response.status(200).send('ok')
-}
-
-function handleMove(request, response) {
-  var gameData = request.body
-
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[3]
-
-  console.log('MOVE: ' + move)
-  response.status(200).send({
-    move: move
-  })
-}
-
-function handleEnd(request, response) {
-  var gameData = request.body
-
-  console.log('END')
-  response.status(200).send('ok')
-}
